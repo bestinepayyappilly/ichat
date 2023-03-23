7f47700523775a7ab0ada9a833ddd6d83d39dcf5
bestinepayyappilly <bestine1234@gmail.com>
2023-03-23 15:03:37 +0530

    added read for firestore

--- src/stacks/HomeStack/Home/Home.tsx
+++ src/stacks/HomeStack/Home/Home.tsx
@@ -17,10 +17,21 @@ const Home = () => {
   const [rooms, setRooms] = useState([]);
   const navigation = useNavigation();
   const getChatRooms = async () => {
-    const chatrooms = await firestore().collection('chatrooms').get();
-    chatrooms.forEach(value => {
-      setRooms(value.data().rooms);
+    const chatrooms = await firestore().collection('users').get();
+    const q = chatrooms.query.orderBy('createdAt', 'desc');
+    const unsubscribe = q.onSnapshot(snapshot => {
+      snapshot.docs.map(e => {
+        console.log(e.data());
+      });
     });
+    return unsubscribe;
+  };
+
+  const createNewChatRooms = (name, image) => {
+    console.log(rooms);
+    firestore()
+      .collection('users')
+      .add({...rooms, image: image, name: name});
   };
   useEffect(() => {
     getChatRooms();
@@ -32,34 +43,40 @@ const Home = () => {
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 45,
       }}>
       <ScrollView contentContainerStyle={{flexDirection: 'row'}} horizontal>
-        {rooms.map((value, index) => {
-          return (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Chats');
-              }}
-              style={{alignItems: 'center'}}>
-              <View
-                style={{
-                  height: 100,
-                  width: 100,
-                  margin: 20,
-                  backgroundColor: 'red',
-                  borderRadius: 100,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  overflow: 'hidden',
-                }}>
-                <Image
-                  source={{uri: value.image}}
-                  style={{height: 100, width: 100}}
-                />
-              </View>
-              <Text>{value.name}</Text>
-            </TouchableOpacity>
-          );
-        })}
+        {rooms &&
+          rooms.map((value, index) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('Chats');
+                }}
+                style={{alignItems: 'center'}}>
+                <View
+                  style={{
+                    height: 100,
+                    width: 100,
+                    margin: 20,
+                    backgroundColor: 'red',
+                    borderRadius: 100,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    overflow: 'hidden',
+                  }}>
+                  <Image
+                    source={{uri: value.image}}
+                    style={{height: 100, width: 100}}
+                  />
+                </View>
+                <Text>{value.name}</Text>
+              </TouchableOpacity>
+            );
+          })}
       </ScrollView>
+      <TouchableOpacity
+        style={{height: 100, width: 100, backgroundColor: 'red'}}
+        onPress={() => {
+          createNewChatRooms('name1', 'imag1e');
+        }}></TouchableOpacity>
     </ParentWrapper>
   );
 };
